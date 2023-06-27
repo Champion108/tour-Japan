d00cdb46bcbf6c26353d1977b6de97676badd43e
Suraj <surajkumar15081947@gmail.com>
2023-06-27 13:12:55 +0530

    favicon

--- script.js
+++ script.js
@@ -9,36 +9,39 @@ const colors = ['rgb(227, 183, 238)', 'rgb(213, 213, 127)', 'rgb(210, 241, 225)'
 let colorIndex = 0;
 
 function changeColor() {
-  gsap.to(element, {
+  gsap.to(element , {
     backgroundColor: colors[colorIndex],
-    duration: 3,
+    duration: 4,
+    ease: Expo.easeInOut,
     onComplete: () => {
       colorIndex = (colorIndex + 1) % 3;
-      changeColor(); 
+      changeColor();
     }
   });
 }
 
-changeColor(); 
+changeColor();
 
 
 tl.to(".imgcont",{
     width:"100%",
     ease: Expo.easeInOut,
-    duration:3,
-    stagger:3,
+    duration:4,
+    stagger:4,
 }, 'miher')
 
+tl
 .to(".txt h1",{
     ease: Expo.easeInOut,
     top:0,
-    duration:3,
-    stagger:3,
+    duration:4,
+    stagger:4,
 }, 'miher')
 
 .to(".txt h1",{
-    delay:3,
+    delay:4,
     ease: Expo.easeInOut,
     top:"-100%" ,
-    stagger:3
+    stagger:4
 }, 'miher')
+
